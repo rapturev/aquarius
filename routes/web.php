3e5260218c83d5e0c93c168278d8d6c2340e08aa
Kirill Monakhov <withewings0@gmail.com>
2025-06-08 22:57:09 +0300

    Fixed some more comments in web.php file

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Route;
 // Home Screen
 Route::view('/', 'home')->name('home');
 
-// Board contain thread group
+// Board contains threads group
 Route::get('/{board}', [ThreadController::class, 'index'])->name('thread.index');
 Route::get('/{board}/res/{thread}', [ThreadController::class, 'show'])->name('thread.show');
 Route::get('/{board}/res/create', [ThreadController::class, 'create'])->name('thread.create');
 Route::post('/{board}/res/store',[ThreadController::class, 'store'])->name('thread.store');
 
-// Thread contain post group
+// Thread contains posts group
 Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 Route::post('/{board}/res/{thread}/post/store', [PostController::class, 'store'])->name('post.store');
